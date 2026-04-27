1d8821420d401eecb02272bc3871227b4660a58a
FlawlessPlagio <jirkaberkamisa@seznam.cz>
2026-04-27 18:14:48 +0200

    Delete MAP--Berka-Week_6/algorithms directory

--- MAP--Berka-Week_6/algorithms/estimate_pose.m
+++
@@ -1,8 +0,0 @@
-function [estimated_pose] = estimate_pose(public_vars)
-    % Nyní bereme střed robota rovnou z Kalmanova filtru (mu)
-    if isfield(public_vars, 'mu') && ~isempty(public_vars.mu)
-        estimated_pose = public_vars.mu;
-    else
-        estimated_pose = nan(1,3);
-    end
-end
